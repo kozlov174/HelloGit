316f3fefa344360f0f9f9a2413b356ac15b11aae
Евгений Куликов <ekueku2001@icloud.com>
2021-03-31 01:52:40 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloGit
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var initialArray = new[] {1, 5, 3, 2, 5, 6};
+            var sortedArray = new[] {1, 2, 3, 5, 5, 6};
+
+            BubbleSort(initialArray);
+
+            if (!initialArray.Intersect(sortedArray).Any()) throw new Exception("Sorting doesn't work!");
+            Console.WriteLine("complete!");
+        }
+
+        private static void BubbleSort(IList<int> array)
+        {
+            // TODO Напиши сортировку пузырьком. Запусти проект, и проверь, что Main не падает.
+        }
+    }
+}
